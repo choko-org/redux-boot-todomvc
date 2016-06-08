ce384f180586248750e679430da1d9dbcf6bcce1
Sebastian Ferrari <sebas@taller.net.br>
2016-06-07 23:23:56 -0300

    Added hotload webpack middleware.

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ import express from 'express'
 const app = new (express)()
 const port = 3000
 
-app.use('/static', express.static('dist'));
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('./webpack.config.js')
+  const compiler = webpack(config)
+
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }))
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve('./index.html'))
